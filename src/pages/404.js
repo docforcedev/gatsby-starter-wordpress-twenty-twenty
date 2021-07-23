21a6caeb3996b35b3faf8750656e67b326a2aaa0
docforcedev <71043751+docforcedev@users.noreply.github.com>
2021-07-23 09:55:54 -0500

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 const NotFound = () => (
   <Layout>
     <div>
-      <h1>Oops, that's a 404</h1>
+      <h1>Oops, that's a 404!</h1>
     </div>
   </Layout>
 )
